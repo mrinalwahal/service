e0be8bdb00d7b7a5fd1806d07e15c192098d5056
Mrinal Wahal <mrinal_w@rocketmail.com>
2024-04-05 05:22:43 +0530

    chore: added logs to service

--- server/http.go
+++ server/http.go
@@ -1,16 +1,18 @@
 package server
 
 import (
-	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/mrinalwahal/service/db"
 	"github.com/mrinalwahal/service/service"
 	"gorm.io/gorm"
+
+	slogGorm "github.com/orandin/slog-gorm"
 )
 
 type HTTPServer struct {
@@ -49,27 +51,38 @@ func NewHTTPServer(config *NewHTTPServerConfig) *HTTPServer {
 
 	if server.logger != nil {
 
-		//	Setup the logger.
+		//	Setup a new logger.
 		server.logger = slog.Default()
 	}
 
+	//
 	//	Add default middlewares.
+	//
+
+	//	Recover middleware recovers from panics anywhere in the chain, prints stack trace and handovers the control to the centralized HTTPErrorHandler.
 	server.echo.Use(middleware.Recover())
 
+	//	Request ID middleware generates a unique ID for every request.
+	server.echo.Use(middleware.RequestID())
+
+	//	RequestLogger middleware allows developer fully to customize what is logged and how it is logged and is more suitable for usage with 3rd party (structured logging) libraries.
 	server.echo.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
 		LogStatus:   true,
 		LogURI:      true,
+		LogMethod:   true,
 		LogError:    true,
 		HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
 		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
 			if v.Error == nil {
-				server.logger.LogAttrs(context.Background(), slog.LevelInfo, "REQUEST",
+				server.logger.LogAttrs(c.Request().Context(), slog.LevelInfo, "HTTP",
+					slog.String("id", c.Response().Header().Get(echo.HeaderXRequestID)),
 					slog.String("method", v.Method),
 					slog.String("uri", v.URI),
 					slog.Int("status", v.Status),
 				)
 			} else {
-				server.logger.LogAttrs(context.Background(), slog.LevelError, "REQUEST_ERROR",
+				server.logger.LogAttrs(c.Request().Context(), slog.LevelError, "HTTP_ERROR",
+					slog.String("id", c.Response().Header().Get(echo.HeaderXRequestID)),
 					slog.String("method", v.Method),
 					slog.String("uri", v.URI),
 					slog.Int("status", v.Status),
@@ -94,7 +107,12 @@ func (s *HTTPServer) InitDefaultRoutes(base string) {
 
 	//	Setup a health check endpoint.
 	group.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "OK")
+		return c.JSON(http.StatusOK, &Response{
+			Message: "Service is up and running.",
+			Data: map[string]interface{}{
+				"request_id": c.Response().Header().Get(echo.HeaderXRequestID),
+			},
+		})
 	})
 
 	//	API v1 routes.
@@ -110,18 +128,213 @@ func (s *HTTPServer) InitDefaultRoutes(base string) {
 // Open database connection and initialize the service.
 func (s *HTTPServer) getService() (service.Service, error) {
 
+	gormLogger := slogGorm.New(
+		slogGorm.WithHandler(s.logger.Handler()), // since v1.3.0
+		slogGorm.WithTraceAll(),                  // trace all messages
+	)
+
 	//	Prepare a database connection.
-	database, err := gorm.Open(s.dialector, &gorm.Config{})
+	database, err := gorm.Open(s.dialector, &gorm.Config{
+		Logger: gormLogger,
+	})
 	if err != nil {
 		return nil, err
 	}
 
-	//	Get the svc.
+	//	Setup the database service.
+	db := db.NewDB(&db.Config{
+		DB: database,
+	})
+
+	//	Setup the business service.
 	svc := service.NewService(&service.Config{
-		DB: db.NewDB(&db.Config{
-			DB: database,
-		}),
+		Logger: s.logger,
+		DB:     db,
 	})
 
 	return svc, nil
+}
+
+//
+//	Handlers
+//
+
+// Create Handler.
+func (s *HTTPServer) create(c echo.Context) error {
+
+	//	Unmarshal the incoming payload.
+	var payload CreateOptions
+	if err := c.Bind(&payload); err != nil {
+		return c.String(http.StatusBadRequest, "Invalid payload.")
+	}
+
+	//	Initialize a default context.
+	ctx := c.Request().Context()
+
+	//	Get the service.
+	svc, err := s.getService()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, &Response{
+			Error: "Failed to either connect to the database or prepare the service.",
+		})
+	}
+
+	//	Call the service function to execute the business logic.
+	todo, err := svc.Create(ctx, &service.CreateOptions{
+		Title: payload.Title,
+	})
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, &Response{
+			Error: "Failed to create the todo.",
+		})
+	}
+
+	return c.JSON(http.StatusOK, &Response{
+		Message: "Todo created successfully.",
+		Data:    todo,
+	})
+}
+
+// Get Handler.
+func (s *HTTPServer) get(c echo.Context) error {
+
+	//	Get the object ID from the URL.
+	id := c.Param("id")
+	uuid, err := uuid.Parse(id)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "Invalid ID.")
+	}
+
+	//	Initialize a default context.
+	ctx := c.Request().Context()
+
+	//	Get the service.
+	svc, err := s.getService()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, &Response{
+			Error: "Failed to either connect to the database or prepare the service.",
+		})
+	}
+
+	//	Call the service function to execute the business logic.
+	todo, err := svc.Get(ctx, uuid)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to create the todo.")
+	}
+
+	return c.JSON(http.StatusOK, &Response{
+		Message: "Todo fetched successfully.",
+		Data:    todo,
+	})
+}
+
+// List Handler.
+func (s *HTTPServer) list(c echo.Context) error {
+
+	//	Unmarshal the incoming payload
+	var payload ListOptions
+	if err := c.Bind(&payload); err != nil {
+		return c.String(http.StatusBadRequest, "Invalid payload.")
+	}
+
+	//	Initialize a default context.
+	ctx := c.Request().Context()
+
+	//	Get the service.
+	svc, err := s.getService()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, &Response{
+			Error: "Failed to either connect to the database or prepare the service.",
+		})
+	}
+
+	//	Call the service function to execute the business logic.
+	todos, err := svc.List(ctx, &service.ListOptions{
+		Skip:           payload.Skip,
+		Limit:          payload.Limit,
+		Title:          payload.Title,
+		OrderBy:        payload.OrderBy,
+		OrderDirection: payload.OrderDirection,
+	})
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to list the todos.")
+	}
+
+	return c.JSON(http.StatusOK, &Response{
+		Message: "Todos fetched successfully.",
+		Data:    todos,
+	})
+}
+
+// Update Handler.
+func (s *HTTPServer) update(c echo.Context) error {
+
+	//	Get the object ID from the URL.
+	id := c.Param("id")
+	uuid, err := uuid.Parse(id)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "Invalid ID.")
+	}
+
+	//	Unmarshal the incoming payload.
+	var payload UpdateOptions
+	if err := c.Bind(&payload); err != nil {
+		return c.String(http.StatusBadRequest, "Invalid payload.")
+	}
+
+	//	Initialize a default context.
+	ctx := c.Request().Context()
+
+	//	Get the service.
+	svc, err := s.getService()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, &Response{
+			Error: "Failed to either connect to the database or prepare the service.",
+		})
+	}
+
+	//	Call the service function to execute the business logic.
+	todo, err := svc.Update(ctx, uuid, &service.UpdateOptions{
+		Title: payload.Title,
+	})
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to create the todo.")
+	}
+
+	return c.JSON(http.StatusOK, &Response{
+		Message: "Todo updated successfully.",
+		Data:    todo,
+	})
+}
+
+// Delete Handler.
+func (s *HTTPServer) delete(c echo.Context) error {
+
+	//	Get the object ID from the URL.
+	id := c.Param("id")
+	uuid, err := uuid.Parse(id)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "Invalid ID.")
+	}
+
+	//	Initialize a default context.
+	ctx := c.Request().Context()
+
+	//	Get the service.
+	svc, err := s.getService()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, &Response{
+			Error: "Failed to either connect to the database or prepare the service.",
+		})
+	}
+
+	//	Call the service function to execute the business logic.
+	err = svc.Delete(ctx, uuid)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to create the todo.")
+	}
+
+	return c.JSON(http.StatusOK, &Response{
+		Message: "Todo deleted successfully.",
+	})
 }
--- service/service.go
+++ service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/google/uuid"
 	"github.com/mrinalwahal/service/db"
@@ -19,6 +20,9 @@ type Config struct {
 
 	//	Database layer service.
 	DB db.DB
+
+	//	Logger.
+	Logger *slog.Logger
 }
 
 // Initializes and gets the service with the supplied database connection.
@@ -32,19 +36,31 @@ type service struct {
 
 	//	Database layer service.
 	db db.DB
+
+	//	Logger.
+	logger *slog.Logger
 }
 
 func (s *service) Create(ctx context.Context, options *CreateOptions) (*db.Todo, error) {
+	s.logger.LogAttrs(ctx, slog.LevelInfo, "SERVICE",
+		slog.String("function", "create"),
+	)
 	return s.db.Create(ctx, &db.CreateOptions{
 		Title: options.Title,
 	})
 }
 
 func (s *service) Get(ctx context.Context, ID uuid.UUID) (*db.Todo, error) {
+	s.logger.LogAttrs(ctx, slog.LevelInfo, "SERVICE",
+		slog.String("function", "get"),
+	)
 	return s.db.Get(ctx, ID)
 }
 
 func (s *service) List(ctx context.Context, options *ListOptions) ([]*db.Todo, error) {
+	s.logger.LogAttrs(ctx, slog.LevelInfo, "SERVICE",
+		slog.String("function", "list"),
+	)
 	return s.db.List(ctx, &db.ListOptions{
 		Title:          options.Title,
 		Skip:           options.Skip,
@@ -55,11 +71,17 @@ func (s *service) List(ctx context.Context, options *ListOptions) ([]*db.Todo, e
 }
 
 func (s *service) Update(ctx context.Context, ID uuid.UUID, options *UpdateOptions) (*db.Todo, error) {
+	s.logger.LogAttrs(ctx, slog.LevelInfo, "SERVICE",
+		slog.String("function", "update"),
+	)
 	return s.db.Update(ctx, ID, &db.UpdateOptions{
 		Title: options.Title,
 	})
 }
 
 func (s *service) Delete(ctx context.Context, ID uuid.UUID) error {
+	s.logger.LogAttrs(ctx, slog.LevelInfo, "SERVICE",
+		slog.String("function", "delete"),
+	)
 	return s.db.Delete(ctx, ID)
 }
