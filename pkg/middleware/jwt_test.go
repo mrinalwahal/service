f51ccc771c14b5d9e28737086525490b7d3dcbcb
Mrinal Wahal <mrinal_w@rocketmail.com>
2024-04-12 11:22:55 +0530

    chore: updated jwt UT

--- pkg/middleware/jwt_test.go
+++ pkg/middleware/jwt_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
 )
 
 func TestJWT(t *testing.T) {
@@ -20,36 +21,40 @@ func TestJWT(t *testing.T) {
 			Key: "secret",
 		})
 
+		// Attach a dummy JWT to the request.
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, JWTClaims{
+			StandardClaims: jwt.StandardClaims{
+				Subject: "3742a2cd-8958-41c1-aba6-ca66c6f3220d",
+				Issuer:  "record",
+			},
+			XUserID: uuid.New(),
+		})
+		signed, err := token.SignedString([]byte("secret"))
+		if err != nil {
+			t.Fatal(err)
+		}
+
 		// Add the middleware to the router.
 		router.Handle("/protected", middleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 			// Read the claims from the request context.
-			_, ok := r.Context().Value(XJWTClaims).(jwt.MapClaims)
-			if !ok {
+			claims, exists := r.Context().Value(XJWTClaims).(JWTClaims)
+			if !exists {
 				http.Error(w, "failed to parse the claims", http.StatusUnauthorized)
 				return
 			}
+
+			if claims.XUserID == uuid.Nil {
+				t.Logf("Claims received: %v", claims)
+				t.Errorf("invalid user_id in claims")
+			}
+
 			w.WriteHeader(http.StatusOK)
 		})))
 
 		// Initialize test r and response recorder.
 		r := httptest.NewRequest(http.MethodGet, "/protected", nil)
 		w := httptest.NewRecorder()
-
-		// Attach a dummy JWT to the request.
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-			"sub": "3742a2cd-8958-41c1-aba6-ca66c6f3220d",
-			"claims": map[string]interface{}{
-				"x-user-id": "3742a2cd-8958-41c1-aba6-ca66c6f3220d",
-			},
-			"aud":   "",
-			"iss":   "record",
-			"scope": "",
-		})
-		signed, err := token.SignedString([]byte("secret"))
-		if err != nil {
-			t.Fatal(err)
-		}
 
 		r.Header.Add("Authorization", signed)
 
